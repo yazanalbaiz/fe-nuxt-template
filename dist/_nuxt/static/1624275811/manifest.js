ff2ea85d6c92a0e247a66557edd526fa5ed1e056
Yazan ALBaiz <yalbaiz@sdaia.gov.sa>
2021-06-21 15:13:47 +0300

    add itemprop to image meta tags

--- dist/_nuxt/static/1624275811/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fen\u002Ftest","\u002F","\u002Fen\u002Finspire","\u002Fen","\u002Finspire","\u002Ftest"]})
--- pages/test.vue
+++ pages/test.vue
@@ -65,6 +65,7 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
+          itemprop: 'image',
           content: this.$config.baseURL + MetaImage,
         },
         {
@@ -90,6 +91,7 @@ export default {
         {
           hid: 'twitter:image',
           name: 'twitter:image',
+          itemprop: 'image',
           content: this.$config.baseURL + MetaImage,
         },
         {
